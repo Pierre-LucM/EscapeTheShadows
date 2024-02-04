97a73e3cc4678f001047023accfaa42ace22397b
Arthur Pineau <arthupineau04@gmail.com>
2024-02-04 13:07:52 +0100

    add sound and activate flash ligh
    !!!Attention!!! active et dasactive la flashlight quand elle est tenu. Voir avec le prof comment recuperer les boutons

--- Assets/Stylized flashlight/Script/FlashLight_Script.cs
+++ Assets/Stylized flashlight/Script/FlashLight_Script.cs
@@ -2,31 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Flashlight : MonoBehaviour
+public class FlashLight : MonoBehaviour
 {
-    public Material lens;
-
     private Light _light;
     private AudioSource _audioSource;
-
     void Start()
     {
-        _light = GetComponentInChildren<Light>();
+        _light = GetComponent<Light>();
         _audioSource = GetComponent<AudioSource>();
+        
     }
     public void LightOn()
     {
         _audioSource.Play();
-        lens.EnableKeyword("_EMISSION");
         _light.enabled = true;
     }
-
-    public void LighOff()
+    public void LightOff()
     {
         _audioSource.Play();
-        lens.DisableKeyword("_EMISSION");
         _light.enabled = false;
-        
     }
-
 }
